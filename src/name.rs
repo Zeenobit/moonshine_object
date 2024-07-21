42ad9d0b0df007d2fdd11947f2a8b5c641fd7fa8
Zeenobit <zeenobit.exe@gmail.com>
2024-07-21 14:16:44 -0400

    Update name.rs

--- src/name.rs
+++ src/name.rs
@@ -3,6 +3,30 @@ use moonshine_kind::prelude::*;
 use crate::{Object, ObjectInstance, ObjectRef};
 
 pub trait ObjectName {
+    /// Returns the [`Name`] of the object.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use bevy::prelude::*;
+    /// # use moonshine_object::prelude::*;
+    ///
+    /// let mut app = App::new();
+    /// // ...
+    /// app.add_systems(Update, print_names);
+    ///
+    /// fn print_names(objects: Objects) {
+    ///     for object in objects.iter() {
+    ///         let entity = object.entity();
+    ///         let name = object.name().unwrap_or("Unnamed");
+    ///         println!("Entity {entity}, Name = {name}");
+    ///     }
+    /// }
+    ///
+    /// # bevy::ecs::system::assert_is_system(print_names);
+    /// ```
+    ///
+    /// [`Name`]: https://docs.rs/bevy/latest/bevy/core/struct.Name.html
     fn name(&self) -> Option<&str>;
 }
 
