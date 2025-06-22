bc98047d1ffae347673a9d9312fee21bed9cef5b
Zeenobit <zeenobit.exe@gmail.com>
2025-06-22 12:44:08 -0400

    Use `KindOf` instead of `CastInto`

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use bevy_ecs::system::SystemParam;
 use moonshine_kind::prelude::*;
 use moonshine_util::hierarchy::HierarchyQuery;
 
-pub use moonshine_kind::{Any, CastInto, Kind};
+pub use moonshine_kind::{Any, Kind, KindOf};
 
 /// A [`SystemParam`] similar to [`Query`] which provides [`Object<T>`] access for its items.
 #[derive(SystemParam)]
